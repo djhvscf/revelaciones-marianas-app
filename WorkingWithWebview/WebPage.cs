036a5acafa13907db3f945df0f7434eb91d5fc6d
Dennis Hernández <dennishernandezvargas@gmail.com>
2017-09-19 18:37:12 -0600

    adding things

---
+++ WorkingWithWebview/WebPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+	public class WebPage : ContentPage
+	{
+		public WebPage ()
+		{
+			var browser = new WebView();
+
+			browser.Source = "https://testrevmarianas.000webhostapp.com/";
+
+			Content = browser;
+		}
+	}
+}
+
